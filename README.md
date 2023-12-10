61a1081a52055e0bbd5b205836058929f17b7849
Aleksei Pershinov <pershinov.alexey@gmail.com>
2023-12-10 18:43:18 +0700

    2022-12-10 - initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# goshmuffle
+simple way to start shell commands from golang.
+use it on your own risk :)
---
+++ goshmuffle.go
@@ -0,0 +1,85 @@
+package goshmuffle
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"os/exec"
+	"syscall"
+)
+
+type result interface {
+	Store(s string)
+}
+
+type runner struct {
+	cmd  string
+	args []string
+	res  result
+
+	exec *exec.Cmd
+}
+
+func New(
+	cmd string,
+	args ...string,
+) *runner {
+	return &runner{
+		cmd:  cmd,
+		args: args,
+	}
+}
+
+func (cr *runner) WithResult(r result) *runner {
+	cr.res = r
+	return cr
+}
+
+func (cr *runner) Run(ctx context.Context) (err error) {
+	cr.exec = exec.CommandContext(ctx, cr.cmd, cr.args...)
+	cr.exec.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	err = cr.exec.Start()
+	if err != nil {
+		return fmt.Errorf("start cmd: %w", err)
+	}
+
+	if cr.res != nil {
+		err = cr.store()
+		if err != nil {
+			return fmt.Errorf("store result: %w", err)
+		}
+	}
+
+	return cr.exec.Wait()
+}
+
+func (cr *runner) store() error {
+	stderr, err := cr.exec.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("stderr pipe: %w", err)
+	}
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		m := scanner.Text()
+		cr.res.Store(m)
+	}
+
+	return nil
+}
+
+func (cr *runner) Terminate() error {
+	if cr.exec == nil {
+		return errors.New("kill: exec is nil")
+	}
+
+	pgid, err := syscall.Getpgid(cr.exec.Process.Pid)
+	if err != nil {
+		return fmt.Errorf("get pid: %w", err)
+	}
+
+	return syscall.Kill(-pgid, 15)
+}
